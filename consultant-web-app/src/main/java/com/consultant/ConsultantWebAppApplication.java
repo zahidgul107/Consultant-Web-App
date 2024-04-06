2195581dcb8dd064c645fb8ac3f7f862befd4a55
ZAHID GUL <98535146+zahidgul107@users.noreply.github.com>
2024-04-06 19:04:33 +0530

    setup web app

---
+++ consultant-web-app/src/main/java/com/consultant/ConsultantWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.consultant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultantWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultantWebAppApplication.class, args);
+	}
+
+}
---
+++ consultant-web-app/src/test/java/com/consultant/ConsultantWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consultant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultantWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
